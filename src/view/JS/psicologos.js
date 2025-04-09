c89bd3065983bc1cfff2e0640b0917cb328a3a31
jedeias_Luiz <jedeiasluiz3@gmail.com>
2025-04-08 21:33:40 -0300

    finalizado a estrutura html da pagina psicologos

---
+++ src/view/JS/psicologos.js
@@ -0,0 +1,65 @@
+
+console.log("Funcionado");
+
+let fousPage = 0;
+
+let hashMap = {
+    0: "null", 
+    1: "anotação",
+    2: "agenda",
+    3: "atividadesRecomendadas"
+}
+
+function nextClick() {
+    fousPage += 1;
+
+    for(let key in hashMap){
+        elemento = document.querySelector("." + hashMap[key]);
+
+        console.log(elemento);
+
+        if (key != fousPage) {
+            elemento = document.querySelector("." + hashMap[key]);
+            elemento.setAttribute("hidden", true);
+
+            console.log(elemento)
+        }
+
+    }
+
+    if(fousPage > 3){
+        fousPage = 1;
+    }
+
+    elemento = document.querySelector("." + hashMap[fousPage])
+    elemento.removeAttribute("hidden")
+
+}
+
+
+
+function prevClick() {
+    fousPage -= 1;
+
+    for(let key in hashMap){
+        elemento = document.querySelector("." + hashMap[key]);
+
+        console.log(elemento);
+
+        if (key != fousPage) {
+            elemento = document.querySelector("." + hashMap[key]);
+            elemento.setAttribute("hidden", true);
+
+            console.log(elemento)
+        }
+
+    }
+
+    if(fousPage < 1){
+        fousPage = 3;
+    }
+
+    elemento = document.querySelector("." + hashMap[fousPage])
+    elemento.removeAttribute("hidden")
+
+}
